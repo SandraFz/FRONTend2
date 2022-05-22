2e4abdfbbe3ad4279b89a435c8277777810294bd
Sandra <sandranfernandez@gmail.com>
2022-05-22 19:43:00 -0300

    Previo a Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:"http://localhost:8080" 
+  baseUrl:"http://https://spring-boot-argentina-programa.herokuapp.com/:8080" 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"http://localhost:8080"
+  baseUrl:"https://spring-boot-argentina-programa.herokuapp.com/"
 };
 
 /*
