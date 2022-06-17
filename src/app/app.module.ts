02c0c32771b22aa824c111e8d58a96cd802f3913
Sandra <sandranfernandez@gmail.com>
2022-06-17 00:13:25 -0300

    Idem.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { PresentationComponent } from './components/presentation/presentation.component';
+import { PresentationComponent } from './components/presentation/presentation.component'
 import { AppRoutingModule } from './app-routing.module';
 import { AdminComponent } from './components/principal/admin/admin.component';
 import { VisitComponent } from './components/principal/visit/visit.component';
