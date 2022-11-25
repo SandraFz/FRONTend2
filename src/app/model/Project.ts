210da646bcd49cc47d695e9ddc0f778e7343826e
Sandra <sandranfernandez@gmail.com>
2022-11-24 23:27:01 -0300

    Corrección de model project.

--- src/app/model/Project.ts
+++ src/app/model/Project.ts
@@ -1,8 +1,8 @@
 export interface Project {
     id_project: number;
     name_project: string;
-    principal: boolean;
     description: string;
+    principal: boolean;
     img_proy: string; 
     logo_img: string;
     link_project: string;
